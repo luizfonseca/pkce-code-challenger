3dcf832ce7e24c94cb54660f9cd848e466d040d8
Luiz Fonseca <eu@luiz.cc>
2019-01-14 18:07:10 +0100

    Second commit with final code.

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const crypto = require('crypto');
+const port = process.env.PORT || 3300;
+const path = require('path')
+const bodyParser = require("body-parser");
+
+const base64URLEncode = (str) => {
+  return str.toString('base64')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=/g, '');
+}
+const sha256 = (buffer) => {
+  return crypto.createHash('sha256').update(buffer).digest();
+}
+app.use(bodyParser());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+// respond with "hello world" when a GET request is made to the homepage
+app.post('/verifier', (req, res) => {
+  res.json({ verifier: base64URLEncode(crypto.randomBytes(32)) })
+})
+
+
+app.post('/challenger', (req, res) => {
+  if (!req.body.verifier) {
+    res.json({ error: "No verifier specified" })
+  }
+
+  res.json({ challenge: base64URLEncode(sha256(req.body.verifier)) })
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
